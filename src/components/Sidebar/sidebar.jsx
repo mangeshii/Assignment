2c594ca5c8fefe00986548e9fd7438f4c9b9eeda
mangeshipatil <thisismangeshi@gmail.com>
2022-09-27 13:18:39 +0530

    Added sidebar icons

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,5 +1,8 @@
 import "./sidebar.css"
 import { HiOutlineHome } from 'react-icons/hi'
+import {FaRegSave} from 'react-icons/fa'
+import {CgFileDocument} from 'react-icons/cg'
+import {TbMessageCircle2} from 'react-icons/tb'
 const Sidebar = () => {
     return (
         <>
@@ -12,20 +15,20 @@ const Sidebar = () => {
                     </div>
                 </a>
                 <a href="#home">
-                    <div className="home-wrapper">
-                        <HiOutlineHome className="home-icon" />
+                    <div className="save-wrapper">
+                        <FaRegSave className="save-icon" />
                         <p className="home-text">Saved</p>
                     </div>
                 </a>
-                <a href="#home">
-                    <div className="home-wrapper">
-                        <HiOutlineHome className="home-icon" />
+                <a className="active" href="#home">
+                    <div className="applied-wrapper">
+                        <CgFileDocument className="applied-icon" />
                         <p className="home-text">Applied</p>
                     </div>
                 </a>
                 <a href="#home">
-                    <div className="home-wrapper">
-                        <HiOutlineHome className="home-icon" />
+                    <div className="message-wrapper">
+                        <TbMessageCircle2 className="message-icon" />
                         <p className="home-text">Messages</p>
                     </div>
                 </a>
