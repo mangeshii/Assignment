ca7cc59d37a50656c6f2f1e0db7d406b8d99d0b5
mangeshipatil <thisismangeshi@gmail.com>
2022-09-27 12:02:10 +0530

    Added navbar

---
+++ src/components/Nav/navbar.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BiSearch } from 'react-icons/bi'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+import { RiAccountCircleFill } from 'react-icons/ri'
+import "./navbar.css"
+
+const NavBar = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg"  className="navbar">
+                <Container>
+                    <Navbar.Brand href="#home">DASHBOARD</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link ><BiSearch className='search'/></Nav.Link>
+                            <Nav.Link ><IoMdNotificationsOutline className='notification'/></Nav.Link>
+                            <Nav.Link ><RiAccountCircleFill className='profile'/></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+export default NavBar
