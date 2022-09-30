41ad6a551e1cfb18c2129bb0120985419fe7d09c
mangeshipatil <thisismangeshi@gmail.com>
2022-09-30 18:25:57 +0530

    added button to save page

--- src/pages/saved.jsx
+++ src/pages/saved.jsx
@@ -1,6 +1,7 @@
 import NavigationBar from "../components/tabs/tabs"
 import "./saves.css"
 import save from "../images/save.png"
+import { NavLink } from 'react-router-dom';
 
 const Saved = () => {
     return (
@@ -12,8 +13,8 @@ const Saved = () => {
             <div className="image-cont container">
                 <img src={save} alt="save" className="save-image img-fluid" />
                 <h5>No jobs saved yet</h5>
-                <p>Jobs you save appear here.
-                </p>
+                <p>Jobs you save appear here.</p>
+                <NavLink to="/jobs"> <button className="find-btn">Find jobs</button></NavLink>
             </div>
         </div>
     )
